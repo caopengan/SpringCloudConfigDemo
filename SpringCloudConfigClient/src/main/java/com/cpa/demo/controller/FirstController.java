49c2e065824b786af3878cf5c199081171325227
曹鹏安 <291033240@qq.com>
2019-12-27 18:28:05 +0800

    spring cloud config client

---
+++ SpringCloudConfigClient/src/main/java/com/cpa/demo/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.cpa.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "hello world";
+    }
+
+
+
+}
---
+++ SpringCloudConfigClient/src/main/java/com/cpa/demo/controller/GitController.java
@@ -0,0 +1,28 @@
+package com.cpa.demo.controller;
+
+import com.cpa.demo.pojo.GitAutoRefreshConfig;
+import com.cpa.demo.pojo.GitConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GitController {
+
+    @Autowired
+    private GitConfig gitConfig;
+
+    @Autowired
+    private GitAutoRefreshConfig gitAutoRefreshConfig;
+
+    @GetMapping(value = "show")
+    public Object show(){
+        return gitConfig;
+    }
+
+    @GetMapping(value = "autoShow")
+    public Object autoShow(){
+        return gitAutoRefreshConfig;
+    }
+
+}
